d370818e97e24fc073cf5be152bb7d72f281c788
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2022-05-08 05:37:28 +0530

    update-frontend-data

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-# project name : sittu application
+# project name          : sittu application
+# project contributions : chathumal/ ishanka/ mindula
+
 import json
 import os
 
@@ -9,72 +11,9 @@ app = Flask(__name__)
 database = "D:\MyWorksSpace\Python Projects\IJSE Workspace\Sittu\db\database.json"
 
 
-@app.route('/hello')
-def hello_world():
+@app.route('/')
+def root():
     return render_template('index.html')
-
-
-def get_data():
-    print("database :", database)
-
-
-# @app.route('/')
-# def main():
-#     return render_template('index.html')
-
-def is_file_empty(file_path):
-    return os.path.exists(file_path) and os.stat(file_path).st_size == 0
-
-
-@app.route('/', methods=["GET", "POST"])
-def register():
-    c_Id = ''
-    name = ''
-    amount = ''
-
-    if request.method == "POST":
-        if "c_Id" in request.form:
-            c_Id = request.form["c_Id"]
-        if "name" in request.form:
-            name = request.form["name"]
-        if "amount" in request.form:
-            amount = request.form["amount"]
-
-    is_empty = is_file_empty(database)
-
-    if is_empty:
-        dict1 = {
-            'Users': [
-                {'id': c_Id,
-                 'name': name,
-                 'Amount': amount,
-                 },
-            ]
-        }
-        out_file = open("db/database.json", "w")
-        json.dump(dict1, out_file, indent=3)
-        out_file.close()
-    else:
-        print('File is not empty')
-
-        def write_json(new_data, filename='db/database.json'):
-
-            with open(filename, 'r+') as file:
-                file_data = json.load(file)
-                file_data["Users"].append(new_data)
-                file.seek(0)
-
-                json.dump(file_data, file, indent=3)
-
-        y = {
-            "id": c_Id,
-            "name": name,
-            "amonut": amount
-        }
-
-        write_json(y)
-
-    return render_template('register.html')
 
 
 if __name__ == '__main__':
